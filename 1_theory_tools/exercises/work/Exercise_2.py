9a1ca36d44c2d3cf58cae04866a7fd76c810be7c
AskerNC <hms467@ku.dk>
2022-02-15 19:34:37 +0100

    Merge https://github.com/bschjerning/dp_ucph

---
+++ 1_theory_tools/exercises/work/Exercise_2.py
@@ -0,0 +1,32 @@
+# import packages used
+import numpy as np
+
+def solve_VFI(par):
+    grid_W = par.grid_W
+    Cstar = np.zeros([par.W+1])
+    
+    # Parameters for VFI
+    max_iter = par.max_iter   # maximum number of iterations
+    delta = 1000 #difference between V_next and V_now
+    tol = par.tol #convergence tol. level
+    it = 0  #iteration counter 
+    V_now = np.zeros([par.W+1]) #arbitrary starting values
+    
+    while (max_iter>= it and tol<delta):
+        it = it+1
+        V_next = V_now.copy()
+        for w in range(par.W+1):
+            c = np.arange(w+1)
+            V_vec = np.sqrt(c)+ par.beta* V_next[w-c]
+            V_now[w] = np.amax(V_vec)  
+            Cstar[w] = np.argmax(V_vec)
+            
+            
+            
+        delta = np.amax(np.abs(V_now - V_next))
+    
+    class sol: pass
+    sol.C = Cstar
+    sol.V = V_now
+
+    return sol
