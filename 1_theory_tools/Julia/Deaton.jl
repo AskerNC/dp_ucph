c9e550ee0fd0f0cb2b5dc14f3b2abb3130933cf6
AskerNC <hms467@ku.dk>
2022-02-22 17:24:55 +0100

    julia_deaton

---
+++ 1_theory_tools/Julia/Deaton.jl
@@ -0,0 +1,101 @@
+module Deaton
+
+using Parameters # For setting default values in struct with @with_kw
+using UnPack # For unpacking 
+using FastGaussQuadrature
+using Distributions
+using Interpolations
+
+"""Class to implement deaton's model with log-normally distrubuted income shocks"""
+
+@with_kw mutable struct deaton_model
+    
+    # structural parameters
+    β::Float64 =0.9     # Discount factor
+    R::Float64 =1       # Returns on savings
+    μ::Float64=0        # Location parameter for income shock, y (if y is log normal, ln(y) ~ N(μ, σ^2))  
+    σ::Float64=1        # Scale parameter for income shock, y
+    η::Float64=1        # CRRA utility parameter, (η=0: linear, η=1: log, η>1: more risk averse than log
+
+    #spaces 
+    xbar::Array=[eps(0.0), 10.] # Upper and lower bound on cash on hand
+    n_x::Int64=50       # Number of grid points for cash on hand
+    n_c::Int64=100      # Number of grid points for choice grid
+    n_y::Int64=10       # Number of quadrature points for income
+    integration::String = "Hermite"
+
+
+    # Spaces dependent on size
+    x::Array = Array{Float64}(LinRange(xbar[1],xbar[2],n_x))
+    c::Array = initiate_c(n_x,n_c,xbar)
+
+    quad::NamedTuple = inititate_integration(n_y,μ,σ , integration)
+
+end
+
+
+function initiate_c(n_x,n_c,xbar)
+    c = Array{Float64}(undef,(n_x,n_c))
+    
+    for i in 1:1:n_x
+        c[i,:] = LinRange(xbar[1],xbar[2],n_c)
+    end
+    return c
+end
+
+
+function inititate_integration(n_y::Int64, μ::Float64, σ::Float64, integration::String )
+    
+    if integration =="Hermite"
+        q, w = gausshermite(n_y)
+        
+        y = exp.(μ .+σ .* sqrt(2) .* q)
+        
+        weights = w ./ sqrt(pi)
+        
+    elseif integration == "Legendre"
+        q, w = gausslegendre(n_y)
+        weights = w ./ 2
+        q_ = (q .+ 1 ) ./2
+        y = quantile.(Normal(μ,σ), q_) 
+    else
+        error("Non-supported integration type")      
+    end
+    
+        
+    return (y = y ,weights =weights)
+end
+
+
+
+function u(dm::deaton_model,c::Array)
+    if dm.η ==1
+        return log.(c)
+    elseif dm.η>=0
+        return (c.^(1-dm.η).-1)./(1 .-dm.η)
+    end 
+
+end
+
+"""
+function bellman(dm::deaton_mode,V0::Array)
+    "Bellman operator, V0 is one-dim vector of values on state grid"
+    interp = LinearInterpolation(dm.x,V0)
+    EV=0
+
+    for (i, y_i) in enumerate(dm.quad.y)
+        x1 = dm.R .* (dm.x .- dm.c ) .+ y_i
+        EV += dm.quad.weights[i] .* interp(x1)
+    end
+
+    matV1 = u(dm,dm.c) .+ dm.β .*EV
+    star = findmax(matV1,2)
+    
+
+            sol.V[iw] = star[1] 
+            sol.C[iw] = (star[2]-1)*w
+
+end
+
+"""
+end
